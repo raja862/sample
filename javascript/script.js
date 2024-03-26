95039b005ac83d5174fff4f4f704ea636b358b46
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2024-03-26 13:41:44 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,65 @@
+
+// var age = 08;
+// var isStudent = false;
+
+
+
+// if (age >= 18 && !isStudent) {
+//     console.log("You are eligible to vote.");
+// } else {
+//     console.log("You are not eligible to vote.");
+// }
+
+
+// //////age user input/////////
+
+// let user=prompt("hello raja")
+
+// let age=parseInt(user)
+
+// if(user>=18){
+//     console.log("you are eligbile to volte")
+// }else{
+//     console.log('you are not eligible')
+// }
+
+// //2 user input maths //////
+
+// let user=+(prompt("enter the string"))
+// let users=+(prompt("enter the string"))
+
+
+// if(isNaN(user) && isNaN(users)){
+//     console.log("this is not a number")
+   
+// }else{
+//     let res=user * users
+
+//     console.log(res)
+// }
+
+
+
+
+// ///////object inverstion/////////
+
+// const obj={a:1, b:2,c:3}
+
+// let res={}
+// for(let key in obj){
+//     res[obj[key]]=key
+// }
+// console.log(res)
+
+// //////avoide dublicate////////
+// let num=[22,33,3,45,55,43,23,11,13,14,56]
+// let num1=[43,12,45,76,87,46,1,11,13,5,22]
+// let num2=[...new Set([...num,...num1])]
+// console.log(num2)
+
+// ////commmon number///////
+
+// const arr1=[2,3,4,5]
+// const arr2=[3,9,7,0]
+// const res=arr1.filter((ele)=>arr2.includes(ele))
+// console.log(res)
